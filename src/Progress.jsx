db509b14492b03fe4d157dfa388a6b4a87b20bed
Kiran Gadamsetty <gadamsettykiran514@gmail.com>
2025-07-28 11:22:36 +0530

    add changes

--- src/Progress.jsx
+++
@@ -1,8 +0,0 @@
-function Progress({percentage}){
-    return(
-<div style  = {{width:"100%", border:"1px solid black", height:"30px"}}>
-<div style = {{width:`${percentage}%`,height:"100%", boxSizing:"border-box", backgroundColor:"red"}}></div>
-</div>
-    )
-}
-export default Progress
